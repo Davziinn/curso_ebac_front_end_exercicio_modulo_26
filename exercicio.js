c413065ebeb662681cedd510f99223080c8a90f4
Davi <mdavi6085@gmail.com>
2024-08-25 15:21:36 -0300

    Exercicio TypeScript

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return 'Olá' + nome;
+}
+console.log(multiplicar(5, 2));
+console.log(saudar('Davi'));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number{
+    return a * b;
+}
+
+function saudar(nome: string): string{
+    return 'Olá' + nome;
+}
+
+console.log(multiplicar(5, 2));
+console.log(saudar('Davi'))
